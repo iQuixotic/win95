834f0b81cca41e5b248a6887e9c9c5db597b3f04
Trey <treyw1850@gmail.com>
2019-05-09 09:39:53 -0400

    kinda works...

--- client/src/components/panel/panel.js
+++ client/src/components/panel/panel.js
@@ -1,12 +1,15 @@
 import * as React from "react";
 import Draggable from 'react-draggable';
-import { Corners } from '../../containers';
+import Resizable from 're-resizable';
+// import { Corners } from '../../containers';
 import './style.css';
 
 const panel = (props) => {
     return (
-            <Draggable>
-                    <div id={props.panelId} className='panel panel-starting-position'>    
+                <Draggable>
+                {/* className='panel-starting-position panel'> */}
+                    <div id={props.panelId} className='panel-starting-position panel'>    
+            <Resizable className='panel'>
                         <div  className='blue-top-box'>
                             <h4 className='panel_header'>{props.head}</h4>
                             <button onClick={props.clickClosed} className='panel_btn'>X</button>
@@ -14,10 +17,11 @@ const panel = (props) => {
                             <button className='panel_btn'>-</button>
                         </div>
                         <iframe title='winder' className='inner-frame' src="http://www.iquixotic.com"></iframe>
-                        <Corners cn='corner top-right'/>
+                        {/* <Corners cn='corner top-right'/>
                         <Corners cn='corner top-left'/>
                         <Corners cn='corner bottom-right'/>
-                        <Corners cn='corner bottom-left'/>
+                        <Corners cn='corner bottom-left'/> */}
+                </Resizable>
                     </div>
             </Draggable>
     );
