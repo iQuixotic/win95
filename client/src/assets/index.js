a3f12a7fad7537b79350c535754319efceb600ea
Trey <treyw1850@gmail.com>
2019-05-16 10:50:46 -0400

    adding new photos

--- client/src/assets/index.js
+++ client/src/assets/index.js
@@ -1,11 +1,25 @@
-// export the icon images
-export { default as RecycleBin_I } from './img/win95Recycle.png';
-export { default as Computer_I } from './img/computerIcon.png';
-export { default as IE_I } from './img/internetExplorer.png';
-export { default as File_I } from './img/fileIcon.png';
-export { default as DialUpGif } from './img/dialupGif.gif';
+// export * from desktop
+export { default as RecycleBin_I } from './img/desktop/win95Recycle.png';
+export { default as Computer_I } from './img/desktop/computerIcon.png';
+export { default as IE_I } from './img/desktop/internetExplorer.png';
+export { default as File_I } from './img/desktop/fileIcon.png';
+
+// export * from panelPics
+export { default as DialUpGif } from './img/panelPics/dialupGif.gif';
+// export { default as DialUpGifPic } from './img/dialupGifPic.gif';
+
+// export * from startMenu
+export { default as Logo } from './img/startMenu/winLogo.png';
 
 
-// export the Windows 95 logo
-export { default as Logo } from './img/winLogo.png';
+// export * from subMenu
+
+export { default as Building } from './img/subMenu/building.png';
+export { default as Cards } from './img/subMenu/cards.png';
+export { default as Cat } from './img/subMenu/cat.png';
+export { default as Chat } from './img/subMenu/chat.png';
+export { default as Portfolio } from './img/subMenu/portfolio.png';
+export { default as Puzzle } from './img/subMenu/puzzle.png';
+
+
 
