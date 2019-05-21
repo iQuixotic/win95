aed5968ffb0838f72d1c4936fe809c7f633d7929
Trey <treyw1850@gmail.com>
2019-05-21 17:09:04 -0400

    added in all backgrounds

--- client/src/components/desktop/icons/computer/computer.js
+++ client/src/components/desktop/icons/computer/computer.js
@@ -20,52 +20,52 @@ const ComputerPanel = (props) => {
                     <p>Red Blocks</p>
                 </div>
 
-                <div  onClick={props.selectBG} className='background-choice'>
+                <div  onClick={props.selectBG} id='green-tiles-bg' className='background-choice'>
                     <img src={GreenTilesBG} alt='#' />
                     <p>Green Tiles</p>
                 </div>
 
-                <div onClick={props.selectBG} className='background-choice'>
+                <div onClick={props.selectBG} id='diamond-wallpaper-bg' className='background-choice'>
                     <img src={DiamondsBG} alt='#' />
                     <p>Diamonds</p>
                 </div>
             </div>
 
             <div className='row'>
-                <div onClick={props.selectBG} className='background-choice'>
+                <div onClick={props.selectBG} id='chains-bg' className='background-choice'>
                     <img src={ChainsBG} alt='#' />
                     <p>Chains</p>
                 </div>
 
-                <div onClick={props.selectBG} className='background-choice'>
+                <div onClick={props.selectBG} id='carved-blocks-bg' className='background-choice'>
                     <img src={CarvedBG} alt='#' />
                     <p>Carved Stone</p>
                 </div>
 
-                <div onClick={props.selectBG} className='background-choice'>
+                <div onClick={props.selectBG} id='blue-prisms-bg' className='background-choice'>
                     <img src={BluePrismsBG} alt='#' />
                     <p>Blue Prisms</p>
                 </div>
 
-                <div onClick={props.selectBG} className='background-choice'>
+                <div onClick={props.selectBG} id='black-white-specs-bg' className='background-choice'>
                     <img src={BlackWhiteSpecsBG} alt='#' />
                     <p>Black and White</p>
                 </div>
             </div>
             <div className='row'>
-                <div className='background-choice'>
+                <div onClick={props.selectBG} id='black-bubbles-bg' className='background-choice'>
                     <img src={BlackBubblesBG} alt='#' />
                     <p>Black Bubbles</p>
                 </div>
-                <div onClick={props.selectBG} className='background-choice'>
+                <div onClick={props.selectBG} id='classic-bg' className='background-choice'>
                 <div className='classic-div'></div>
                     <p>Windows 95 Classic</p>
                 </div>
-                <div onClick={props.selectBG} className='background-choice'>
+                <div onClick={props.selectBG} id='purple-bg' className='background-choice'>
                 <div className='purple-div'></div>
                     <p>Plain Purple</p>
                 </div>
-                <div onClick={props.selectBG} className='background-choice'>
+                <div onClick={props.selectBG} id='green-bg' className='background-choice'>
                     <div className='green-div'></div>
                     <p>Plain Green</p>
                 </div>
