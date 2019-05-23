faa266378ea36c7b6de89a44dd17614ddf7731ec
Trey <treyw1850@gmail.com>
2019-05-23 01:19:18 -0400

    styled the contact panel and added in links

--- client/src/components/menu/contact/contact.js
+++ client/src/components/menu/contact/contact.js
@@ -7,11 +7,11 @@ const ContactPanel = (props) => {
             <div className='contact-panel'>
                 <div className='contact-panel-content'>
                     <img src={Cat} alt='#' />
-                    <p>Email: <span className='contact-panel-info'>treyw1850@gmail.com</span></p>
-                    <p>Phone: <span className='contact-panel-info'>(334) 342-8086</span></p>
-                    <p>Github: <span className='contact-panel-info'>httpsomethingsomething..</span></p>
-                    <p>Linkedin: <span className='contact-panel-info'>httpsomethinglinkedin..</span></p>
-                    <p>Alternate-portfolio: <span className='contact-panel-info'>www.iquixotic.com</span></p>
+                    <p><span className='subj'> Email: </span><span className='contact-panel-info'>treyw1850@gmail.com</span></p>
+                     <p><span className='subj'> Phone: </span><span className='contact-panel-info'>(334) 342-8086</span> </p>
+                     <p><span className='subj'> Github: </span> <a className='contact-panel-info' href='https://github.com/iQuixotic' alt='#'>https://github.com/iQuixotic </a></p>
+                     <p><span className='subj'> Linkedin: </span><a className='contact-panel-info' href='https://www.linkedin.com/in/trey-warrick-928547169' alt='#'>https://www.linkedin.com/in/trey-warrick-928547169 </a></p>
+                     <p><span className='subj'> Alternate Portfolio: </span> <a  className='contact-panel-info' href='http://www.iquixotic.com' alt='#'>http://www.iquixotic.com </a></p>
                 </div>
                 <div className='cancel-btn'>                    
                     <button className='windows-btn' onClick={props.clickClosed} ><span>Got it!!!</span></button>
